921874e0a8d84bcdf4f5da32f768c46d52f1895d
LuckHost <sh76484@gmail.com>
2024-11-21 15:22:11 +0700

    first lab created

---
+++ 2.R
@@ -0,0 +1,52 @@
+num <- seq(1, 37)
+a <- c(0.46, 0.52, 0.86, 0.91, 0.54, 
+       0.64, 0.67, 0.74, 0.69, 0.61, 
+       0.65, 0.53, 0.76,
+       0.41, 0.33, 0.41, 0.28, 0.39, 
+       0.36, 0.40, 0.67, 0.39, 0.33, 
+       0.56, 0.61, 0.18, 0.59, 0.23,
+       0.09, 0.22, 0.27, 0.40, 0.27,
+       0.06, 0.30, 0.36, 0.42)
+b <- c("Северный", "Северный", "Северный", 
+       "Северный", "Северный", "Северный", 
+       "Северный", "Северный", "Северный", 
+       "Северный", "Северный", "Северный", "Северный",
+       "Средняя полоса", "Средняя полоса", "Средняя полоса",
+       "Средняя полоса", "Средняя полоса", "Средняя полоса",
+       "Средняя полоса", "Средняя полоса", "Средняя полоса",
+       "Средняя полоса", "Средняя полоса", "Средняя полоса",
+       "Средняя полоса", "Средняя полоса", "Средняя полоса",
+       "Южный", "Южный", "Южный",
+       "Южный", "Южный", "Южный",
+       "Южный", "Южный", "Южный")
+
+b_factor <- factor(b)
+
+my_frame <- data.frame(num = num, measurement = a, climate_region = b_factor)
+
+print(my_frame)
+print(dim(my_frame))        # Размерность таблицы
+print(str(my_frame))        # Структура таблицы
+print(names(my_frame))      # Названия признаков
+head(my_frame, 4)           # Первые 4 строки таблицы
+
+subset(my_frame, measurement > 0.2 & measurement < 0.7)
+subset(my_frame, climate_region == "Северный")
+my_frame[c(1, 3, 6, 9, 10), ]
+my_frame[my_frame$measurement == min(my_frame$measurement), ]
+
+new_rows <- data.frame(num = c(16, 17, 18), 
+                       measurement = c(0.5, 0.4, 0.6), 
+                       climate_region = factor(c("Северный", "Средняя полоса", "Южный")))
+my_frame <- rbind(my_frame, new_rows)
+
+
+mean_val <- mean(my_frame$measurement)
+sd_val <- sd(my_frame$measurement)
+
+write.table(data.frame(mean = mean_val, sd = sd_val), "mean_sd.txt", row.names = FALSE)
+
+my_frame$New <- rnorm(n = nrow(my_frame), mean = mean_val, sd = sd_val)
+print(my_frame)
+
+
